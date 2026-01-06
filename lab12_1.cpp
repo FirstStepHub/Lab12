a509bca5fd065ade1df37037dd59674524e37deb
FirstStepHub <first_yo1@hotmail.com>
2026-01-06 20:25:43 +0700

    lab12_1_complete.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
-using namespace std;
+using namespace std ;
+
+long long int fibonacci(int x) ;
 
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+long long fibonacci(int x){
+    if (x == 0) return 0 ;
+    if (x == 1) return 1 ;
+    
+    return fibonacci(x-1) + fibonacci(x-2) ;
 }
